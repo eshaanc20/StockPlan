0b30766e100b8db8f52401b16b5759a849dac917
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-10-04 20:11:11 -0400

    Improved css for navigation

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MenuModule } from '../menu/menu.module';
-
+import { NavigationModule } from '../navigation/navigation.module';
 
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    MenuModule
+    MenuModule,
+    NavigationModule
   ],
   exports: [DashboardComponent]
 })
