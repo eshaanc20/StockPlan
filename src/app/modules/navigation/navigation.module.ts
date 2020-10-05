fd8b3fa217a6bf5a9fdaf9845d51d1d0e5ebcd86
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-10-04 21:31:59 -0400

    Added stock lists option to navigation module

--- src/app/modules/navigation/navigation.module.ts
+++ src/app/modules/navigation/navigation.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 
 
 @NgModule({
   declarations: [NavigationComponent],
   imports: [
     CommonModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [NavigationComponent]
 })
