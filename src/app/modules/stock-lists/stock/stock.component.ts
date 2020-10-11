be06b0dcd724c657dbeb6d8bd900990c69940bc3
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-10-11 18:01:34 -0400

    Added more information to stock component

--- src/app/modules/stock-lists/stock/stock.component.ts
+++ src/app/modules/stock-lists/stock/stock.component.ts
@@ -10,11 +10,12 @@ interface StockInformationFormat {
   change: string;
   percentChange: string;
   amountChange: number;
-  name: string;
-  currency: string;
-  exchange: string;
+  high52Week: number;
+  low52Week: number;
   marketCap: number;
-  outstanding: number;
+  eps: number;
+  dividendYield: number;
+  profitEarningRatio: number;
 }
 
 @Component({
@@ -26,11 +27,23 @@ export class StockComponent implements OnInit {
   @Input() stock: StockInformationFormat;
   priceColor: string;
   priceIncrease: boolean;
+  marketCap: number;
+  marketCapDetail: string;
 
   constructor() { }
 
   ngOnInit() {
     this.priceColor = this.stock.change === 'increase' ? 'green' : 'red';
     this.priceIncrease = this.stock.change === 'increase' ? true : false;
+    if (this.stock.marketCap > 1000) {
+      this.marketCap = (Math.round(this.stock.marketCap / 10)) / 100;
+      this.marketCapDetail = 'T';
+    } else if (this.stock.marketCap > 0) {
+      this.marketCap = (Math.round(this.stock.marketCap * 100)) / 100;
+      this.marketCapDetail = 'B';
+    } else {
+      this.marketCap = (Math.round(this.stock.marketCap / 100)) * 100;
+      this.marketCapDetail = 'M';
+    }
   }
 }
