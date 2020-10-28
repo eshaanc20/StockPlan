047d57d89b117d378d8847eecd34cdcdbfff94bf
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-10-27 23:15:36 -0400

    Added component for summary

--- src/app/modules/portfolio/portfolio.module.ts
+++ src/app/modules/portfolio/portfolio.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortfolioDetailComponent } from './portfolio-detail/portfolio-detail.component';
+import { SummaryComponent } from './summary/summary.component';
+import { MatTableModule } from '@angular/material';
 
 
 
 @NgModule({
-  declarations: [PortfolioDetailComponent],
+  declarations: [PortfolioDetailComponent, SummaryComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule
   ],
   exports: [PortfolioDetailComponent]
 })
