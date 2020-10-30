92d8e1d4545de9e7c2bacbe8051c593c11796754
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-10-29 20:32:50 -0400

    Added table for portfolio summary

--- src/app/modules/portfolio/summary/summary.component.ts
+++ src/app/modules/portfolio/summary/summary.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PortfolioStock } from '../../interfaces';
 
 @Component({
   selector: 'app-summary',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./summary.component.css']
 })
 export class SummaryComponent implements OnInit {
+  @Input() data: PortfolioStock[];
+  tableColumns = ['Symbol', 'Shares', 'Book Value', 'Market Value', 'Change Amount', 'Change'];
 
   constructor() { }
 
