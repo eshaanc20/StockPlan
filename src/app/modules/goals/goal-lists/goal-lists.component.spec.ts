1774883bce37d718b6053b4a021be8fcc6a0285b
Eshaan Chaudhari <eshaanchaudhari@Eshaans-MacBook-Pro.local>
2020-11-13 17:35:11 -0500

    Updated angular packages

--- src/app/modules/goals/goal-lists/goal-lists.component.spec.ts
+++ src/app/modules/goals/goal-lists/goal-lists.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GoalListsComponent } from './goal-lists.component';
 
@@ -6,7 +6,7 @@ describe('GoalListsComponent', () => {
   let component: GoalListsComponent;
   let fixture: ComponentFixture<GoalListsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GoalListsComponent ]
     })
--- src/app/modules/stock-lists/new-list/new-list.component.spec.ts
+++ src/app/modules/stock-lists/new-list/new-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NewListComponent } from './new-list.component';
 
@@ -6,7 +6,7 @@ describe('NewListComponent', () => {
   let component: NewListComponent;
   let fixture: ComponentFixture<NewListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NewListComponent ]
     })
